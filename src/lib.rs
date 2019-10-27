9ec3a346885bf9be128cde03a83f03ba3b1cc873
Rowell Belen <developer@bytekast.com>
2019-10-27 09:48:36 -0600

    Use strong typed events

--- src/lib.rs
+++ src/lib.rs
@@ -6,29 +6,24 @@ extern crate serde_json;
 
 use aws_lambda_events::event::apigw::*;
 use js_sys::Promise;
-use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 #[allow(unused_variables)]
 pub fn hello(event: JsValue, context: JsValue) -> Promise {
-    let http_event: ApiGatewayProxyRequest = event.into_serde().unwrap();
+    let http_event: ApiGatewayProxyRequest = event.into_serde().unwrap(); // convert JsValue to Rust type
     let data = json!({
         "message": "Hello, WebAssembly!",
         "input": http_event
     });
 
-    let response = Response {
-        statusCode: 200,
-        body: serde_json::to_string_pretty(&data).unwrap(),
+    let response = ApiGatewayProxyResponse {
+        status_code: 200,
+        body: serde_json::to_string_pretty(&data).ok(),
+        headers: Default::default(),
+        multi_value_headers: Default::default(),
+        is_base64_encoded: None
     };
-    let js_result = serde_wasm_bindgen::to_value(&response).unwrap();
+    let js_result = serde_wasm_bindgen::to_value(&response).unwrap(); // convert Rust type to JsValue
     return Promise::resolve(&js_result);
-}
-
-#[derive(Serialize, Deserialize)]
-#[allow(non_snake_case)]
-pub struct Response {
-    pub statusCode: u16,
-    pub body: String,
 }
